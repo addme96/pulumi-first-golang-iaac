ea4d36fc0b90be0f174868b2042a47620c784385
Dawid Dudek <addme96@gmail.com>
2021-12-12 21:54:31 +0100

    created frontend image

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		stack := os.Getenv(pulumi.EnvStack)
 		getwd, _ := os.Getwd()
+
 		backendImageName := "backend"
-		_, err := docker.NewImage(ctx, "backend", &docker.ImageArgs{
+		backend, err := docker.NewImage(ctx, "backend", &docker.ImageArgs{
 			ImageName: pulumi.String(fmt.Sprintf("%v:%v", backendImageName, stack)),
 			Build: docker.DockerBuildArgs{
 				Context: pulumi.String(path.Join(getwd, "app", "backend")),
@@ -25,6 +26,20 @@ func main() {
 			return err
 		}
 
+		frontendImageName := "frontend"
+		frontend, err := docker.NewImage(ctx, "frontend", &docker.ImageArgs{
+			ImageName: pulumi.String(fmt.Sprintf("%v:%v", frontendImageName, stack)),
+			Build: docker.DockerBuildArgs{
+				Context: pulumi.String(path.Join(getwd, "app", "frontend")),
+			},
+			Registry: docker.ImageRegistryArgs{},
+			SkipPush: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+
+		fmt.Print(backend.ImageName, frontend.ImageName)
 		return nil
 	})
 
